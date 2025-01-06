14233193b7354133bda0bb53f2866641952be712
Kristoffer Åberg <kristoffer.aberg81@gmail.com>
2025-01-06 14:49:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -46,5 +46,5 @@ def download(filename):
         return jsonify({'error': 'Filen hittades inte'}), 404
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 1337))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=True)
