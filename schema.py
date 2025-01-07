10375ebf8944eb6848d2623bada406eb9a3aab6a
Kristoffer Åberg <kristoffer.aberg81@gmail.com>
2025-01-07 22:30:18 +0100

    Update schema.py

--- schema.py
+++ schema.py
@@ -136,7 +136,7 @@ def todate(date_str, time_str):
         time_str = time_str.replace(":", "")[:4]
     return f"{date_str}T{time_str}00Z"
 
-def geticsfor(domain, school_name, unit_guid, school_year, larare, epost):
+def geticsfor(domain, school_name, unit_guid, school_year, larare, email):
     log_message("Startar processen för att skapa ICS-fil")
     s = requests.session()
     weeks = {}
